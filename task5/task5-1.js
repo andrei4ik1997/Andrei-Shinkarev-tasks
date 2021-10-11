6717dd5159fdd5266978a50baf0ed7e419f5a73a
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-11 22:57:48 +0300

    add deep clone object

---
+++ task5/task5-1.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const etalonObject = {
+  name: "Andrei",
+  age: 24,
+  location: {
+    country: "Belarus",
+    city: "Mogilev",
+  },
+  technologyStack: {
+    JS: {
+      core: "basic",
+      frameworks: "React",
+    },
+    Html: "HTML5",
+    Css: "CSS3",
+  },
+};
+
+function cloneObject(obj) {
+  const clone = {};
+  for (const key in obj) {
+    if (typeof obj[key] === "object" && obj[i] != null) {
+      clone[key] = cloneObject(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+  return clone;
+}
+
+const copyObject = cloneObject(etalonObject);
+copyObject.location.city = "Minsk";
+copyObject.hobbies = "Voleyball";
+console.log("Etalon Object:", etalonObject);
+console.log("Copy Object:", copyObject);
