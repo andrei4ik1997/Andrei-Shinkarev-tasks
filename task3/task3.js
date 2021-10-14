cfb020389fc89a2acafe621f9f34d8ba11928fd1
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-14 17:49:40 +0300

    fix: delete 'else' statement and add 'const' keyword.

--- task3/task3.js
+++ task3/task3.js
@@ -3,32 +3,31 @@
 function stringParse(string) {
   if (typeof string !== "string") {
     return "Please enter a valid string";
-  } else {
-    const letterArray = string.split(""),
-      resultArray = [];
-    for (let i = 0; i < letterArray.length; i++) {
-      if (letterArray[i] !== letterArray[i + 1]) {
-        resultArray.push(letterArray[i]);
-      } else {
-        resultArray.push(letterArray[i]);
-        resultArray.push(letterArray[i + 1]);
-        if (letterArray[i] === letterArray[i + 2]) {
-          const duplicateLetter = letterArray[i];
-          let pointer = i + 1;
-          resultArray.push("[");
-          while (duplicateLetter === letterArray[pointer + 1]) {
-            resultArray.push(letterArray[i]);
-            pointer++;
-          }
-          resultArray.push("]");
-          i = pointer;
-        } else {
-          i = i + 1;
+  }
+  const letterArray = string.split("");
+  const resultArray = [];
+  for (let i = 0; i < letterArray.length; i++) {
+    if (letterArray[i] !== letterArray[i + 1]) {
+      resultArray.push(letterArray[i]);
+    } else {
+      resultArray.push(letterArray[i]);
+      resultArray.push(letterArray[i + 1]);
+      if (letterArray[i] === letterArray[i + 2]) {
+        const duplicateLetter = letterArray[i];
+        let pointer = i + 1;
+        resultArray.push("[");
+        while (duplicateLetter === letterArray[pointer + 1]) {
+          resultArray.push(letterArray[i]);
+          pointer++;
         }
+        resultArray.push("]");
+        i = pointer;
+      } else {
+        i = i + 1;
       }
     }
-    return resultArray.join("");
   }
+  return resultArray.join("");
 }
 
 console.log(stringParse("aaaabbcdefffffffg"));
