02cce13ef03278a1d622e29debebb5b6edc69e2a
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-14 17:54:01 +0300

    fix: fix case with no arguments

--- task3/task2.js
+++ task3/task2.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const wordPattern = function (word) {
-  let letterArray = word.toLowerCase().split(""),
-    arr = [...new Set(letterArray)];
+  if (!arguments.length) return `You haven't entered anything`;
+  let letterArray = word.toLowerCase().split("");
+  let arr = [...new Set(letterArray)];
   for (let i = 0; i < letterArray.length; i++) {
     letterArray[i] = arr.indexOf(letterArray[i]);
   }
