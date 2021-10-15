4a07e3ef70ec1585ac15f35fbdccd51fd484dde1
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-15 14:49:55 +0300

    fix: fix cases with empty string and strings including non-alphabetic characters

--- task3/task2.js
+++ task3/task2.js
@@ -1,16 +1,28 @@
 "use strict";
 
 const wordPattern = function (word) {
-  if (!arguments.length) return `You haven't entered anything`;
-  let letterArray = word.toLowerCase().split("");
-  let arr = [...new Set(letterArray)];
-  for (let i = 0; i < letterArray.length; i++) {
-    letterArray[i] = arr.indexOf(letterArray[i]);
+  if (typeof word !== "string") {
+    return `Please enter a valid string value, but you enter a ${typeof word} value`;
   }
-  return letterArray.join(".");
+  let letterArray = word.toLowerCase().split("");
+  const onlyString = letterArray.some((elem) => Number.isInteger(+elem));
+  if (!onlyString) {
+    let arr = [...new Set(letterArray)];
+    for (let i = 0; i < letterArray.length; i++) {
+      letterArray[i] = arr.indexOf(letterArray[i]);
+    }
+    return letterArray.join(".");
+  } 
+  return `Please enter a string value without number`;
 };
 
+console.log(wordPattern());
+console.log(wordPattern(12));
+console.log(wordPattern(undefined));
+console.log(wordPattern(null));
+console.log(wordPattern(true));
 console.log(wordPattern("hello"));
 console.log(wordPattern("heLlo"));
 console.log(wordPattern("helLo"));
+console.log(wordPattern("helLo1234"));
 console.log(wordPattern("Hippopotomonstrosesquippedaliophobia"));
