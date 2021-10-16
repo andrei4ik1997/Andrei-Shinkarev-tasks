2168da2d039fdcaf9fca3fc9bd98e35df4e9b00d
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-16 15:35:12 +0300

    fix: fix bug

--- task5/task5-1.js
+++ task5/task5-1.js
@@ -20,7 +20,7 @@ const etalonObject = {
 function cloneObject(obj) {
   const clone = {};
   for (const key in obj) {
-    if (typeof obj[key] === "object" && obj[i] != null) {
+    if (typeof obj[key] === "object" && obj[key] != null) {
       clone[key] = cloneObject(obj[key]);
     } else {
       clone[key] = obj[key];
