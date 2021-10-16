25121339706634f48020f1051bf91de8d970c89e
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-16 15:16:55 +0300

    modify: modify apply func

--- task4/task4-3.js
+++ task4/task4-3.js
@@ -4,18 +4,28 @@ const person = {
   name: "Andrei",
 };
 
-function myApply(func, context, args) {
-  context.applyFunc = func;
-  const result = context.applyFunc(...args);
-  delete context.applyFunc;
-  return result;
-}
+const person2 = {
+  name: "Maxim",
+};
 
 function sayHello(surname, age) {
   return `Hello ${this.name} ${surname}, age: ${age}`;
 }
 
-console.log(myApply(sayHello, person, ["Shinkarev", 24]));
-console.log(myApply(sayHello, person, ["Ivanov", 42]));
-console.log(myApply(sayHello, person, ["Petrov", 37]));
-console.log(myApply(sayHello, person, ["Sidorov", 27]));
+Function.prototype.apply = function (context, args) {
+  const randomValue = Date.now().toString();
+  context.randomValue = this;
+  const result = context.randomValue(...args);
+  delete context[randomValue];
+  return result;
+};
+
+console.log(sayHello.apply(person, ["Shinkarev", 24]));
+console.log(sayHello.apply(person2, ["Shinkarev", 24]));
+console.log(sayHello.apply(person, ["Ivanov", 42]));
+console.log(sayHello.apply(person2, ["Ivanov", 24]));
+console.log(sayHello.apply(person, ["Petrov", 37]));
+console.log(sayHello.apply(person2, ["Petrov", 24]));
+console.log(sayHello.apply(person, ["Sidorov", 27]));
+console.log(sayHello.apply(person2, ["Sidorov", 24]));
+
