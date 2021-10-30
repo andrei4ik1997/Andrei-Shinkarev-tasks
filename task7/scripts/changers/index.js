07fa971a5d5bd34717baa26e23e019dae9e2da05
andrei4ik1997 <andrey03071997@yandex.ru>
2021-10-31 01:02:40 +0300

    add: webpack pattern

---
+++ task7/scripts/changers/index.js
@@ -0,0 +1,4 @@
+import { changeDate } from "./changeDate";
+import { changeSettings } from "./changeSettings";
+
+export { changeDate, changeSettings };
---
+++ task7/scripts/scheduler/index.js
@@ -0,0 +1,9 @@
+import { addNote } from "./addNote";
+import { closeScheduler } from "./closeScheduler";
+import { createNotes } from "../creators/createNotes";
+import { deleteNote, deleteAllNotes } from "./deleteNote";
+import { editNote } from "./editNote";
+import { scheduler } from "./scheduler";
+import { showScheduler } from "./showScheduler";
+
+export { addNote, closeScheduler, createNotes, deleteNote, deleteAllNotes, editNote, scheduler, showScheduler };
