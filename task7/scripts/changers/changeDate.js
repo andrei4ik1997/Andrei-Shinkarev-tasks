65d1876759697d451d5c4e6cfde7cb5fd28b327f
andrei4ik1997 <andrey03071997@yandex.ru>
2021-11-01 21:23:13 +0300

    fix: use delegation

--- task7/scripts/changers/changeDate.js
+++ task7/scripts/changers/changeDate.js
@@ -3,11 +3,8 @@ import { date } from "../../index";
 
 export function changeDate() {
   const calendarClock = document.querySelector(".calendar__clock");
-  const calendarDateDay = document.querySelector(".calendar__dateDay");
-  const calendarDateMonth = document.querySelector(".calendar__dateMonth");
-  const calendarDateYear = document.querySelector(".calendar__dateYear");
-  const calendarDateControlsArrowPrev = document.querySelector(".calendar__dateControlsArrow_prev");
-  const calendarDateControlsArrowNext = document.querySelector(".calendar__dateControlsArrow_next");
+  const calendarDateContainer = document.querySelector(".calendar__dateContainer");
+  const calendarDateControls = document.querySelector(".calendar__dateControls");
 
   calendarClock.addEventListener("click", () => {
     date.setDate(new Date().getDate());
@@ -16,31 +13,31 @@ export function changeDate() {
     makeCalendar();
   });
 
-  calendarDateDay.addEventListener("change", (e) => {
-    date.setDate(+e.target.value);
-    date.setMonth(date.getMonth());
-    date.setFullYear(date.getFullYear());
+  calendarDateContainer.addEventListener("change", (e) => {
+    const value = +e.target.value;
+    switch (e.target.dataset.action) {
+      case "day":
+        date.setDate(value);
+        break;
+      case "month":
+        date.setMonth(value);
+        break;
+      case "year":
+        date.setFullYear(value);
+        break;
+    }
     makeCalendar();
   });
 
-  calendarDateMonth.addEventListener("change", (e) => {
-    console.log(+e.target.value)
-    date.setMonth(+e.target.value);
-    makeCalendar();
-  });
-
-  calendarDateYear.addEventListener("change", (e) => {
-    date.setFullYear(e.target.value);
-    makeCalendar();
-  });
-
-  calendarDateControlsArrowPrev.addEventListener("click", () => {
-    date.setMonth(date.getMonth() + 1);
-    makeCalendar();
-  });
-
-  calendarDateControlsArrowNext.addEventListener("click", () => {
-    date.setMonth(date.getMonth() - 1);
+  calendarDateControls.addEventListener("click", (e) => {
+    switch (e.target.dataset.action) {
+      case "prevMonth":
+        date.setMonth(date.getMonth() + 1);
+        break;
+      case "nextMonth":
+        date.setMonth(date.getMonth() - 1);
+        break;
+    }
     makeCalendar();
   });
 }
