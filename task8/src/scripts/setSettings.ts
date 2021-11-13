693f5a23f157d4e6ba81470130ad5bf7759e2a28
andrei4ik1997 <andrey03071997@yandex.ru>
2021-11-13 16:41:21 +0300

    add: scripts

---
+++ task8/src/scripts/setSettings.ts
@@ -0,0 +1,26 @@
+interface Config {
+  firstDayWeek: string;
+  firstHoliday: string;
+  secondHoliday: string;
+  showLastDays: boolean;
+  showNextDays: boolean;
+  scheduler: boolean;
+}
+
+export function setSettings(config: Config) {
+  const { firstDayWeek, firstHoliday, secondHoliday, showLastDays, showNextDays, scheduler } = config;
+
+  const settingsSelectFirstDay: HTMLInputElement = document.querySelector(".settings__select_first-day");
+  const settingsHolidayFirst: HTMLInputElement = document.querySelector(".settings__holidayFirst");
+  const settingsHolidaySecond: HTMLInputElement = document.querySelector(".settings__holidaySecond");
+  const settingsInputLastDays: HTMLInputElement = document.querySelector(".settings__input_lastDays");
+  const settingsInputNextDays: HTMLInputElement = document.querySelector(".settings__input_nextDays");
+  const settingsInputScheduler: HTMLInputElement = document.querySelector(".settings__input_scheduler");
+
+  settingsSelectFirstDay.value = firstDayWeek;
+  settingsHolidayFirst.value = firstHoliday;
+  settingsHolidaySecond.value = secondHoliday;
+  settingsInputLastDays.checked = showLastDays;
+  settingsInputNextDays.checked = showNextDays;
+  settingsInputScheduler.checked = scheduler;
+}
