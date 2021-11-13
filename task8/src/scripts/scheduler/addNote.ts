814cbcf15c3bac349eeaf32a4cf45c36985a2d13
andrei4ik1997 <andrey03071997@yandex.ru>
2021-11-13 16:38:25 +0300

    add: sheduler scripts

---
+++ task8/src/scripts/scheduler/addNote.ts
@@ -0,0 +1,49 @@
+import { createNotes } from "../creators/index";
+import { month, urlApi } from "../utils/index";
+
+interface Note {
+  year: number;
+  month: number;
+  day: number;
+  value: string;
+}
+
+export function addNote(): void {
+  const addForm: HTMLFormElement = document.querySelector(".scheduler__form");
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formInput: HTMLInputElement = document.querySelector(".form__input");
+    const modalDate: HTMLElement = document.querySelector(".scheduler__date");
+    const dateString: Array<string> = modalDate.innerText.split(" ");
+    const date: Date = new Date(
+      +dateString[2],
+      month.findIndex((month) => month === dateString[1]),
+      +dateString[0]
+    );
+
+    const obj: Note = {
+      day: date.getDate(),
+      month: date.getMonth(),
+      year: date.getFullYear(),
+      value: formInput.value,
+    };
+
+    fetch(`${urlApi}/notes`, postMethod(obj))
+      .then((res) => res.json())
+      .then(() => {
+        addForm.reset();
+        createNotes(date);
+      })
+      .catch((err) => console.log(err));
+  });
+}
+
+const postMethod = (obj: Note) => {
+  return {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(obj),
+  };
+};
---
+++ task8/src/scripts/scheduler/deleteNote.ts
@@ -0,0 +1,27 @@
+import { createNotes } from "../creators/index";
+import { urlApi } from "../utils/index";
+
+export function deleteNote(params: { date: Date; id: number }) {
+  const { date, id } = params;
+
+  fetch(`${urlApi}/notes/${id}`, deleteMethod)
+    .then((response) => response.json())
+    .then(() => createNotes(date))
+    .catch((err) => console.log(err));
+}
+
+export function deleteAllNotes() {
+  const deleteAll: HTMLElement = document.querySelector(".scheduler__button");
+
+  deleteAll.addEventListener("click", () => {
+    const iconTrashAll: Array<HTMLElement> = Array.from(document.querySelectorAll(".fa-trash"));
+    iconTrashAll.forEach((elem: HTMLElement) => elem.click());
+  });
+}
+
+const deleteMethod = {
+  method: "DELETE",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+};
