65b6027ec40594aef38421d6716ad7d96aa2eae0
andrei4ik1997 <andrey03071997@yandex.ru>
2021-11-13 16:37:19 +0300

    add: utils scripts

---
+++ task8/src/scripts/utils/constantValues.ts
@@ -0,0 +1,16 @@
+export const weekDays: Array<string> = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+export const month: Array<string> = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export const urlApi: string = "http://localhost:3000";
