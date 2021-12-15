b0fa70c93d8996479e58ab339aa3083d1f68afdc
andrei4ik1997 <andrey03071997@yandex.ru>
2021-12-15 10:47:27 +0300

    add const values

---
+++ AngularProject/MobileHill/src/app/shared/values.ts
@@ -0,0 +1,14 @@
+export const brands: string[] = [
+  'Samsung',
+  'Apple',
+  'Xiaomi',
+  'HONOR',
+  'POCO',
+  'Huawei',
+  'Realme',
+  'OnePlus',
+  'Google',
+  'Nokia',
+  'Asus',
+];
+export const memories: string[] = ['16', '32', '64', '128', '256', '512'];
