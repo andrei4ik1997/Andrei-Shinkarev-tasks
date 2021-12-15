e4feab5534eb74ff7adba5221f1be03bf4e0ce43
andrei4ik1997 <andrey03071997@yandex.ru>
2021-12-15 10:17:30 +0300

    init project

---
+++ AngularProject/MobileHill/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+import { Environment } from 'src/app/shared/interfaces';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAoYY7eba73PS4lysI_7i-oVDYCcIFkBDs',
+  fireBaseDataBaseUrl:
+    'https://mobilehill-fa3df-default-rtdb.europe-west1.firebasedatabase.app',
+};
