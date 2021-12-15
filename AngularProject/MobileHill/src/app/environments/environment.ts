471ed54f6b9b231dddcb122310be66e0754da312
andrei4ik1997 <andrey03071997@yandex.ru>
2021-12-15 10:19:37 +0300

    add environment

---
+++ AngularProject/MobileHill/src/app/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import { Environment } from 'src/app/shared/interfaces';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyAoYY7eba73PS4lysI_7i-oVDYCcIFkBDs',
+  fireBaseDataBaseUrl:
+    'https://mobilehill-fa3df-default-rtdb.europe-west1.firebasedatabase.app',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
