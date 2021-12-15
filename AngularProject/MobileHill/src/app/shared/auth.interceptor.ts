20388fda0cb25ea32bd3fa99bb3d67beb4d71ea3
andrei4ik1997 <andrey03071997@yandex.ru>
2021-12-15 10:45:02 +0300

    add auth interceptor

---
+++ AngularProject/MobileHill/src/app/shared/auth.interceptor.ts
@@ -0,0 +1,43 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, Observable, throwError } from 'rxjs';
+
+import { AuthService } from '../admin/services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (this.auth.isAuthenticated()) {
+      request = request.clone({
+        setParams: {
+          auth: this.auth.token,
+        },
+      });
+    }
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.auth.logout();
+          this.router.navigate(['/admin', 'login'], {
+            queryParams: {
+              authFailed: true,
+            },
+          });
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
